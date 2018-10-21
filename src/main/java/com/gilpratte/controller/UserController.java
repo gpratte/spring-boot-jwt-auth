04ab69fc7fe6e9b65169cf675b11ac0f86393879
Gil Pratte <gil.pratte@mutualmobile.com>
2018-10-21 18:52:09 -0500

    testing Unauthorized 401 response

--- src/main/java/com/gilpratte/controller/UserController.java
+++ src/main/java/com/gilpratte/controller/UserController.java
@@ -3,10 +3,7 @@ package com.gilpratte.controller;
 import com.gilpratte.model.User;
 import com.gilpratte.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,12 +13,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value="/user", method = RequestMethod.GET)
+    @GetMapping(value="/user")
     public List<User> listUser(){
         return userService.findAll();
     }
 
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/user/{id}")
     public User getOne(@PathVariable(value = "id") Long id){
         return userService.findById(id);
     }
