0eea4f9765bd3a08dc48458224a74c118ff78e59
Gil Pratte <gil.pratte@mutualmobile.com>
2018-11-14 19:55:17 -0600

    test the generate-token endpoint

--- README.md
+++ README.md
@@ -3,16 +3,29 @@
 Based on the blog entry Spring Boot [Security Jwt Authentication](https://www.devglan.com/spring-security/spring-boot-jwt-auth) by Dhiraj.
 
 Modernizing the code by
+* It compiles!
 * Use Lombok for model classes
 * Changing from MySQL to an embedded H2 database
 * Added a CommandLineRunner to initialize the database
 * Using @SpringBootTest to call the endpoints
 
-## How to Run
+## Maven
+Make sure you have maven installed.
+
+To check run the following from the command line
+* *mvn --version*
+You should see something starting with _Apache Maven_ and a lot more output.
+
+## IntelliJ
 If running in IntelliJ then remember to add the lombok plugin and enable the annotation.
 
-Run from the tests command line
+## How to Run the tests
+Run from the tests from the command line
 * *mvn clean test*
 
+## How to Run the server
 Run from the application from the command line
 * *mvn clean spring-boot:run*
+
+## curl
+There is a curl folder that has a curl command to call the generate token endpoint.
--- src/test/java/com/gilpratte/controller/ControllerTest.java
+++ src/test/java/com/gilpratte/controller/ControllerTest.java
@@ -1,16 +1,30 @@
 package com.gilpratte.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
+/**
+ * Test the controller endpoints
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
@@ -21,10 +35,49 @@ public class ControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Test
     public void authenticationNoToken() {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/user",
             String.class)).contains("Unauthorized").contains("401");
+    }
+
+    /**
+     * This test calls the generate token endpoint the same as the follow curl does
+ <pre>curl -X POST localhost:8080/token/generate-token \
+ -H "Content-Type: application/json" \
+ -d '{"username": "Alex123", "password": "password"}'</pre>
+     * and expects the result to have json like the following
+ <pre>{"token":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBbGV4MTIzIiwic2NvcGVzIjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpc3MiOiJodHRwOi8vZ2lscHJhdHRlLmNvbSIsImlhdCI6MTU0MjI0NTQwOCwiZXhwIjoxNTQyMjYzNDA4fQ.eqf9V_vkVB2AoMkKBwIpyFAAWnWcmkkbmY7Ygb3m6Lw"}</pre>
+     */
+    @Test
+    public void generateToken() throws IOException {
+        // Headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Body
+        Map<String, String> body = new HashMap<String, String>();
+        body.put("username", "Alex123");
+        body.put("password", "password");
+        String bodyAsJson = mapper.writeValueAsString(body);
+
+        // Headers and body
+        HttpEntity<String> requestEnty = new HttpEntity<>(bodyAsJson, headers);
+
+        String url = "http://localhost:" + port + "/token/generate-token";
+
+        // Call the endpoint
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, requestEnty, String.class);
+
+        // Turn the json reply into a map
+        TypeReference<HashMap<String, String>> typeRef
+            = new TypeReference<HashMap<String, String>>() {};
+        Map<String, String> responseToken = mapper.readValue(responseEntity.getBody(), typeRef);
+
+        // Make sure there is a key with the name "token"
+        assertThat(responseToken.keySet()).contains("token");
     }
 
 }
